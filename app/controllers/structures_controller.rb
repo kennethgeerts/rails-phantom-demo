791704202f3a69446c089b1c069bc1b30344b613
Kenneth Geerts <kenneth.geerts@up-nxt.com>
2017-10-10 10:27:22 +0200

    Base64 image

--- app/controllers/structures_controller.rb
+++ app/controllers/structures_controller.rb
@@ -6,10 +6,16 @@ class StructuresController < ApplicationController
       format.html
       format.json { render json: @structure.to_json }
       format.png do
-        send_data StructureImage.new(@structure).data,
+        send_data StructureImage.new(@structure).data(show_params),
           type: 'image/png',
           disposition: 'inline'
       end
     end
+  end
+
+  private
+
+  def show_params
+    params.permit(:base64)
   end
 end
--- app/models/structure_image.rb
+++ app/models/structure_image.rb
@@ -5,10 +5,10 @@ class StructureImage
     @structure = structure
   end
 
-  def data
+  def data(opts = {})
     phantom_file_path = File.expand_path('phantom.js', __dir__)
     cmd = "phantomjs #{phantom_file_path}"
     stdout, _ = Open3.capture2(cmd, stdin_data: @structure.to_json)
-    Base64.decode64(stdout)
+    opts[:base64].present? ? stdout : Base64.decode64(stdout)
   end
 end
